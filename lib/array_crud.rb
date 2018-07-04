0ae973e756ad999c8b693d1a07e389bbf68a8e28
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 12:05:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [apple, orange, pear, banana]
+  my_array = [apple, orange, pear, banana]
 end
 
 def add_element_to_end_of_array(array, element)
